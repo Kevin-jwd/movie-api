da58eff7fe824c82b5d61c18abe5c450a0b611f6
DESKTOP-D0SBUEF\USER <herobine159@naver.com>
2025-02-19 00:33:36 +0900

    feat: mariadb 연동 및 전체 영화 조회 API 추가

--- server.js
+++ server.js
@@ -1,9 +1,23 @@
 const express = require("express");
 const app = express();
+const mariadb = require("mysql");
+
 const port = 1234;
+const conn = mariadb.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "root",
+    database: "Movie",
+});
 
 app.use(express.json());
 
 app.listen(port, () => {
     console.log(`서버가 포트 번호 ${port}에서 실행 중입니다.`);
+});
+
+// GET /movies - 전체 영화 조회
+app.get("/", (req, res) => {
+    res.send("Hello Movie API!");
 });
